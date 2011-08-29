fe3d7759ec682028edeca651e200c512637fca77
Amos Benari <abenari@redhat.com>
2011-08-29 09:22:02 +0300

    fixed organization selector related tests.

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,21 @@
+ require 'spec_helper'
+
+describe ApplicationController do
+  include LoginHelperMethods
+  include MockHelperMethods
+
+  before (:each) do
+    login_user
+  end
+
+  describe "set_org" do
+    it 'should set the current organization in the session' do
+      org = real_org()
+      Organization.should_receive(:find).with(org.key).and_return(org)
+      request.env['HTTP_REFERER'] = "/"
+      post :set_org, :workingorg => org.key
+      session[:current_organization_id].should == org.key
+    end
+  end
+end
+ 
