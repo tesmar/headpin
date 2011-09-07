a666c8280836f0f5a67bb5a579be8f75f5181352
Tom McKay <thomasmckay@redhat.com>
2011-09-07 12:17:33 -0400

    rspec GET index for /subscriptions

--- spec/controllers/subscriptions_controller_spec.rb
+++ spec/controllers/subscriptions_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe SubscriptionsController do
+  include LoginHelperMethods
+  include MockHelperMethods
+
+  before (:each) do
+    login_user
+  end
+
+  describe "GET index" do
+    it 'should be successful' do
+      org = real_org
+      get 'index', {}, {:current_organization_id => org.key}
+      response.should be_success
+    end
+  end
+
 
 end
